94be29b1e768c67afefde40644af96d58b0c91f4
mnltake <minolu2003@ybb.ne.jp>
2021-10-22 08:51:59 +0900

    create new repo

---
+++ shutdownd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+reTerminal user button "o" -> shutdown
+$ sudo pip3 install seeed-python-reterminal
+"""
+import os
+import seeed_python_reterminal.core as rt
+import seeed_python_reterminal.button as rt_btn
+device = rt.get_button_device()
+while 1:
+  for event in device.read_loop():
+    buttonEvent = rt_btn.ButtonEvent(event)
+    if buttonEvent.name ==  rt_btn.ButtonName.O:
+      os.system("sudo shutdown -h now")
+
